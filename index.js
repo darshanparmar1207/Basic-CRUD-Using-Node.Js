57a2ded719e07545cd2f75717ccb3825838de864
Darshan Parmar <parmardarshanr1@gmail.com>
2026-07-18 23:23:35 +0530

    Refactor user routes and clean up code structure

--- index.js
+++ index.js
@@ -1,51 +1,47 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 const userModel = require('./usermodel');
 
-app.get('/',(req, res) => {
+app.get('/', (req, res) => {
     res.send("hey");
 })
 
-// its a create user 
-app.get('/create', async (req, res) => {
-   let createduser = await userModel.create({
-        name: "hel",
-        username: "hel",
-        email: "hel@gmail.com"
-    })
-
-    res.send(createduser)
+// create user
+app.post('/create', async (req, res) => {
+    let { name, username, email } = req.body;
+    let createduser = await userModel.create({ name, username, email });
+    res.send(createduser);
 })
 
-// its a update user 
-app.get('/update', async (req, res) => {
-    
-     let updateduser = await userModel.findOneAndUpdate({username: "hello"},
-    {name: "hii"}, {new: true})
+// read all users
+app.get("/read", async (req, res) => {
+    let users = await userModel.find();
+    res.send(users);
+})
+
+// read one user by id
+app.get("/readone/:id", async (req, res) => {
+    let user = await userModel.findOne({ _id: req.params.id });
+    res.send(user);
+})
+
+// update user by id
+app.post("/update/:id", async (req, res) => {
+    let updateduser = await userModel.findOneAndUpdate(
+        { _id: req.params.id },
+        req.body,
+        { new: true }
+    );
     res.send(updateduser);
 })
 
-// its read all user data 
-app.get("/read",  async (req, res) => {
-    let users = await userModel.find()
-    res.send(users)
+// delete user by id
+app.get("/delete/:id", async (req, res) => {
+    let deleteduser = await userModel.findOneAndDelete({ _id: req.params.id });
+    res.send(deleteduser);
 })
-
-// its read one user data 
-app.get("/readone",  async (req, res) => {
-    let users = await userModel.findOne({username: "hel"});
-    res.send(users)
-})
-
-// its delete user data 
-app.get("/delete",  async (req, res) => {
-    let users = await userModel.findOneAndDelete({username: "hello"});
-    res.send(users)
-})
-
-
-
-
 
 app.listen(3000);
